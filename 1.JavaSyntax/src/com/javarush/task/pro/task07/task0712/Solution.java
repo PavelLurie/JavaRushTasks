6bab2b5f57a242666caee15b5a4224783436989d
79313 <paul.lurie@yandex.ru>
2023-02-18 12:35:40 +0300

    Уровень 7 задача 12

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String a = new String ("Привет");
+        String b = new String ("как");
+        String c = new String ("дела");
+        String d = new String ("Hello");
+        String e = new String ("Hi");
+        String f = new String ("my");
+        String g = new String ("name");
+        String h = new String ("Java");
+        String i = new String ("слово");
+        String j = new String ("какое-то");
+        int [] array1 = new int[2];
+        int [] array2 = new int[3];
+        int [] array3 = new int[4];
+        int [] array4 = new int[5];
+        int [] array5 = new int[6];
+        Scanner console = new Scanner (System.in);
+        Scanner console1 = new Scanner (System.in);
+        
+        
+        
+    }
+}
