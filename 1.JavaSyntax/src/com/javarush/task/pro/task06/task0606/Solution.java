28dfdc06ff98d15396ea31e5756bdd59140123b8
79313 <paul.lurie@yandex.ru>
2023-02-18 12:21:29 +0300

    Уровень 6 задача 6

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(int a, int b){
+    }
+
+    public static void universalMethod(int [] c, int g){
+    }
+
+    public static void universalMethod(int [][] c, int g){
+    }
+
+    public static void universalMethod(String [] c, int g){
+    }
+
+    public static void universalMethod(String c, int g){
+    }
+    
+    //
+
+    public static void universalMethod(int a, double b){
+    }
+
+    public static void universalMethod(int [] c, double[] g){
+    }
+
+    public static void universalMethod(int [][] c, String g){
+    }
+
+    public static void universalMethod(String [] c, char g){
+    }
+
+    public static void universalMethod(String c, boolean g){
+    }
+}
+
