0f276fba1a48e0aff5ba5662d5cc243a0176e89a
79313 <paul.lurie@yandex.ru>
2023-02-18 12:31:18 +0300

    Уровень 7 задача 1

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    byte a = 12;
+    short b = 128;
+    int  c = 1234;
+    long d = 123456;
+    float e = 3.45f;
+    double f = 4.56;
+    boolean g = true;
+    char h = '%';
+}
