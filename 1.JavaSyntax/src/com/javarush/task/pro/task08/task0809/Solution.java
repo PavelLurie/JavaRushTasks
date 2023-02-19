5148645daa93513cd53042c877e69569872c3bf4
79313 <paul.lurie@yandex.ru>
2023-02-19 12:52:33 +0300

    Уровень 8 задача 9

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0809/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.pro.task08.task0809;
+
+/* 
+Counter
+
+*/
+public class Solution {
+
+    public static void main(String[] args) throws InterruptedException {
+        printNumbers();
+    }
+
+    public static void printNumbers() throws InterruptedException {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        int e = 5;
+        int f = 6;
+        int g = 7;
+        int h = 8;
+        int i = 9;
+        int j = 10;
+        
+        System.out.println(a);
+        Thread.sleep(1, 300000);
+        System.out.println(b);
+        Thread.sleep(1, 300000);
+        System.out.println(c);
+        Thread.sleep(1, 300000);
+        System.out.println(d);
+        Thread.sleep(1, 300000);
+        System.out.println(e);
+        Thread.sleep(1, 300000);
+        System.out.println(f);
+        Thread.sleep(1, 300000);
+        System.out.println(g);
+        Thread.sleep(1, 300000);
+        System.out.println(h);
+        Thread.sleep(1, 300000);
+        System.out.println(i);
+        Thread.sleep(1, 300000);
+        System.out.println(j);
+        
+    }
+}
