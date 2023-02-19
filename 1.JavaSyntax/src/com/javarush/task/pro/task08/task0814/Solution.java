1e5a4f9a5dcef50e535c241c3e0c60f820450b26
79313 <paul.lurie@yandex.ru>
2023-02-19 12:54:25 +0300

    Уровень 8 задача 14

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0814/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task08.task0814;
+
+/* 
+Флаги
+*/
+
+public class Solution {
+
+    public static int setFlag(int number, int flagPos) {
+        int i = number | (1 << flagPos);
+        return i;
+    }
+
+    public static int resetFlag(int number, int flagPos) {
+        int i = number & ~(1 << flagPos);
+        return i;
+    }
+
+    public static boolean checkFlag(int number, int flagPos) {
+        boolean i = (number & (1 << flagPos)) == (1 << flagPos);
+        return i;
+    }
+}
