cdc1322e0076a8d42f173252f259c2f1cd649d10
79313 <paul.lurie@yandex.ru>
2023-02-21 12:12:17 +0300

    Уровень 13 задача 5

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1305/Main.java
@@ -0,0 +1,10 @@
+package com.javarush.task.pro.task13.task1305;
+
+public class Main {
+    public static void main(String[] args) {
+        String line = "BuG";
+        String newLine = line.toLowerCase();
+
+        System.out.println(newLine);
+    }
+}
