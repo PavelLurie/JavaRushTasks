7c6554a57ebe2e8353aee067b0bd6dc146b94f73
79313 <paul.lurie@yandex.ru>
2023-02-21 12:03:32 +0300

    Уровень 12 задача 15

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1215/Main.java
@@ -0,0 +1,33 @@
+package com.javarush.task.pro.task12.task1215;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        String glassTop     = "       🍷       ";
+        String glassSecond  = "      🍷🍷      ";
+        String glassThird   = "     🍷🍷🍷     ";
+        String glassFourth  = "    🍷🍷🍷🍷    ";
+        String glassFifth   = "   🍷🍷🍷🍷🍷   ";
+        String glassSixth   = "  🍷🍷🍷🍷🍷🍷  ";
+        String glassSeventh = " 🍷🍷🍷🍷🍷🍷🍷 ";
+        String glassEighth  = "🍷🍷🍷🍷🍷🍷🍷🍷";
+        ArrayList<String> list = new ArrayList<>();
+        list.add(glassEighth);
+        list.add(glassSeventh);
+        list.add(glassSixth);
+        list.add(glassFifth);
+        list.add(glassFourth);
+        list.add(glassThird);
+        list.add(glassSecond);
+        list.add(glassTop);
+
+        System.out.println(list);
+
+        for (int i = list.size() - 1; i >= 0; i--) {
+            list.remove(i);
+        }
+
+        System.out.println(list);
+    }
+}
