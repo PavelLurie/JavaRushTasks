56756c7a0294218df049903b5978d49f476dff71
79313 <paul.lurie@yandex.ru>
2023-02-21 12:07:09 +0300

    Уровень 12 задача 19

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1219/Main.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task12.task1219;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        var strings = new ArrayList<String>()
+
+        {{
+            add("Ghbnjn");
+        }};
+    }
+}
