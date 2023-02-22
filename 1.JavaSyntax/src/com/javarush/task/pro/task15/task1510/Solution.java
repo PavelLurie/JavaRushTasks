42f886d21baba36f138f6edc6871a29985ea7be8
79313 <paul.lurie@yandex.ru>
2023-02-22 12:57:39 +0300

    Уровень 15 задача 10

--- 1.JavaSyntax/src/com/javarush/task/pro/task15/task1510/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1510/Solution.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /* 
@@ -17,7 +18,7 @@ public class Solution {
         try (InputStream stream = System.in;
              Scanner scanner = new Scanner(stream);
              BufferedWriter bufferedWriter = Files.newBufferedWriter(Path.of(scanner.nextLine()))) {
-            bufferedWriter.write(bytes);
+            bufferedWriter.write(Arrays.toString(bytes));
         } catch (IOException e) {
             System.out.println("Something went wrong : " + e);
         }
