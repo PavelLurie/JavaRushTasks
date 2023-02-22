bdadb5c88a209bcaed7e09d964cca4866b953e0c
79313 <paul.lurie@yandex.ru>
2023-02-22 12:55:32 +0300

    Уровень 15 задача 9

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1509/Main.java
@@ -0,0 +1,28 @@
+package com.javarush.task.pro.task15.task1509;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        try (InputStream stream = System.in;
+             Scanner console = new Scanner(stream)) {
+            String line = console.nextLine();
+            char[] chars = line.toCharArray();
+            Set<Character> characters = new HashSet<>();
+            for (char aChar : chars) {
+                if (Character.isAlphabetic(aChar)) {
+                    characters.add(Character.toLowerCase(aChar));
+                }
+            }
+            System.out.println(characters);
+        } catch (IOException e) {
+            System.out.println("Something went wrong : " + e);
+        }
+    }
+}
