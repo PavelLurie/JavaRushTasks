634d0fca613999396392850889d7279aa1d6416b
79313 <paul.lurie@yandex.ru>
2023-02-23 11:43:35 +0300

    Уровень 16 задача 11

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1611/Main.java
@@ -0,0 +1,10 @@
+package com.javarush.task.pro.task16.task1611;
+
+import java.time.LocalTime;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalTime localTime = LocalTime.MIDNIGHT;
+        System.out.println(localTime);
+    }
+}
