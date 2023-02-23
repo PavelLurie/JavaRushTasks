4e0dc1284b26ce788f529b6c5fb2f0c8d4ce0c86
79313 <paul.lurie@yandex.ru>
2023-02-23 11:59:39 +0300

    Уровень 17 задача 17

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1717/Main.java
@@ -0,0 +1,9 @@
+package com.javarush.task.pro.task17.task1717;
+
+public class Main {
+    public static void main(String[] args) {
+        Raven raven = new Raven();
+        int temp = raven.getMaxSpeed();
+        System.out.println(temp);
+    }
+}
