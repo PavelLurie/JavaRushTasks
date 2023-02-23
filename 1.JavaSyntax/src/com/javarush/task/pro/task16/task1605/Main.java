a4bdff02383d4cd291905ed0bf7a1e6c5056c515
79313 <paul.lurie@yandex.ru>
2023-02-23 11:40:44 +0300

    Уровень 16 задача 5

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1605/Main.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task16.task1605;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Main {
+    public static final Calendar INVENTION_DAY = new GregorianCalendar(2192, Calendar.JUNE, 12);
+    public static void main(String[] args) {
+        boolean temp = false;
+
+
+    }
+}
