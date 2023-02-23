0bb79ae1478bf47b25e1a912e4a09d9a4932b477
79313 <paul.lurie@yandex.ru>
2023-02-23 11:48:10 +0300

    Уровень 16 задача 20

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1620/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task16.task1620;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+/* 
+Еще один простой шаблон
+*/
+
+public class Solution {
+
+    static ZonedDateTime zonedDateTime = ZonedDateTime.now();
+
+    public static void main(String[] args) {
+        DateTimeFormatter now = DateTimeFormatter.ofPattern("e d.M.yy HH:mm:ss.n VV");
+        String text = now.format(zonedDateTime);
+        System.out.println(text);
+    }
+}
+/*
+ DateTimeFormatter now = DateTimeFormatter.ofPattern("dd.MM.yyyy" + "г." + " " + "H" + "ч." + "m" + "мин");
+ String text = now.format(localDateTime);
+ */
