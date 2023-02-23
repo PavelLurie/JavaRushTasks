f06beb365bbada673c558c330197eda0298e6252
79313 <paul.lurie@yandex.ru>
2023-02-23 11:45:07 +0300

    Уровень 16 задача 14

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1614/Main.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task16.task1614;
+
+import java.time.Instant;
+
+public class Main {
+    public static void main(String[] args) {
+        Instant instant = Instant.MAX;
+
+        System.out.println(instant);
+    }
+}
