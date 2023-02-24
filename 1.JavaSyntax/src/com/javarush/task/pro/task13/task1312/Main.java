1a1a8d134a3d8ef24f4469dd65b701edf268854d
79313 <paul.lurie@yandex.ru>
2023-02-24 12:30:13 +0300

    Уровень 13 задача 12

--- 1.JavaSyntax/src/com/javarush/task/pro/task13/task1312/Main.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1312/Main.java
@@ -4,22 +4,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Main {
-    public static HashMap<Integer, String> getProgrammingLanguages() {
-        HashMap<Integer, String> map = new HashMap<Integer, String>();
-        map.put(0, "Java");
-        map.put(1, "Kotlin");
-        map.put(2, "Go");
-        map.put(3, "Javascript");
-        map.put(4, "Typescript");
-        map.put(5, "Python");
-        map.put(6, "PHP");
-        map.put(7, "C++");
-
-        return map;
-
-    }
-
     public static void main(String[] args) {
-        System.out.println(getProgrammingLanguages());
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("6");
+        strings.add("Hello");
     }
 }
