5f76191c966e7c7be08437aade33f51fda2c71ff
79313 <paul.lurie@yandex.ru>
2023-04-10 20:47:21 +0300

    Уровень 18 задача 4

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1804/Solution.java
@@ -0,0 +1,53 @@
+package com.javarush.task.task18.task1804;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/* 
+Самые редкие байты
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        String path = scanner.nextLine();
+        FileInputStream inputStream = new FileInputStream(path);
+        ArrayList<Integer> list = new ArrayList<>();
+        while (inputStream.available() > 0) {
+            int next_byte = inputStream.read();
+            list.add(Integer.valueOf(String.valueOf(next_byte)));
+        }
+        inputStream.close();
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (Integer temp : list) {
+            if (!map.containsKey(temp)) {
+                map.put(temp, 1);
+            } else {
+                map.put(temp, map.get(temp) + 1);
+            }
+        }
+
+        Collection<Integer> value = map.values();
+        ArrayList<Integer> key = new ArrayList<>();
+        int g = Integer.MAX_VALUE;
+        for (Integer s : value){
+            if (g >= s){
+                g = s;
+                key.add(g);
+            }
+        }
+
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            for (int i = 0; i < key.size(); i++) {
+                if(entry.getValue() == g) {
+                    System.out.print(entry.getKey() + " ");
+                    break;
+                }
+            }
+
+        }
+    }
+}
