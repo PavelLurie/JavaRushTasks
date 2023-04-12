d8b71531f2a758a76f7074286db22d4612cf3785
79313 <paul.lurie@yandex.ru>
2023-04-12 16:05:37 +0300

    Уровень 18 задача 13

--- 2.JavaCore/src/com/javarush/task/task18/task1813/AmigoOutputStream.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1813/AmigoOutputStream.java
@@ -1,23 +1,26 @@
 package com.javarush.task.task18.task1813;
 
-import java.io.*;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /* 
 AmigoOutputStream
 */
 
-public class AmigoOutputStream extends FileOutputStream{
+public class AmigoOutputStream extends FileOutputStream {
     public static String fileName = "C:/tmp/result.txt";
+
     FileOutputStream fileOutputStream;
 
     public AmigoOutputStream(FileOutputStream fileOutputStream) throws FileNotFoundException {
         super(fileName);
         this.fileOutputStream = fileOutputStream;
     }
+
     public static void main(String[] args) throws FileNotFoundException {
         new AmigoOutputStream(new FileOutputStream(fileName));
     }
-
 
     @Override
     public void write(int b) throws IOException {
@@ -32,6 +35,11 @@ public class AmigoOutputStream extends FileOutputStream{
     @Override
     public void write(byte[] b, int off, int len) throws IOException {
         fileOutputStream.write(b, off, len);
+    }
+
+    @Override
+    public void flush() throws IOException {
+        fileOutputStream.flush();
     }
 
     @Override
