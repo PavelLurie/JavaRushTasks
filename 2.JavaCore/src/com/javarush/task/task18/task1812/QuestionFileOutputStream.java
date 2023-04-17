d0cc05470e406c5ff30db2be29d43896b83a792f
79313 <paul.lurie@yandex.ru>
2023-04-17 16:48:27 +0300

    Уровень 18 задача 27

--- 2.JavaCore/src/com/javarush/task/task18/task1812/QuestionFileOutputStream.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1812/QuestionFileOutputStream.java
@@ -9,42 +9,30 @@ import java.io.InputStreamReader;
 */
 
 public class QuestionFileOutputStream implements AmigoOutputStream {
-    private AmigoOutputStream amigoOutputStream;
-    private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    public QuestionFileOutputStream(AmigoOutputStream amigoOutputStream) {
-        this.amigoOutputStream = amigoOutputStream;
-    }
-
 
     @Override
     public void flush() throws IOException {
-        amigoOutputStream.flush();
+
     }
 
     @Override
     public void write(int b) throws IOException {
-        amigoOutputStream.write(b);
+
     }
 
     @Override
     public void write(byte[] b) throws IOException {
-        amigoOutputStream.write(b);
+
     }
 
     @Override
     public void write(byte[] b, int off, int len) throws IOException {
-        amigoOutputStream.write(b, off, len);
+
     }
 
     @Override
     public void close() throws IOException {
-        System.out.println("Вы действительно хотите закрыть поток? Д/Н");
-        String question = br.readLine();
 
-        if (question.equals("Д")){
-            amigoOutputStream.close();
-        }
     }
 }
 
