f10210e7200ea46c4bc7d47049bb2fbae549f2a7
79313 <paul.lurie@yandex.ru>
2023-04-19 22:08:20 +0300

    Уровень 18 задача 22

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1822/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task18.task1822;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+/* 
+Поиск данных внутри файла
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+
+        try (BufferedReader bufferedFileReader = new BufferedReader(new FileReader(reader.readLine()))) {
+            String str = bufferedFileReader.readLine();
+            while (str != null) {
+                if (str.startsWith(args[0] + " ")) {
+                    System.out.println(str);
+                    break;
+                }
+            }
+        }
+    }
+}
