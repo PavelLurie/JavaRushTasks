7e543822d1256758cf843ede9aac6166627fc043
79313 <paul.lurie@yandex.ru>
2023-05-15 15:53:46 +0300

    Уровень 20 задача 3

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
@@ -0,0 +1,47 @@
+package com.javarush.task.task20.task2003;
+
+import java.io.*;
+import java.util.*;
+
+/* 
+Знакомство с properties
+*/
+
+public class Solution {
+
+    public static Map<String, String> runtimeStorage = new HashMap<>();
+
+    public static void save(OutputStream outputStream) throws Exception {
+        Properties properties = new Properties();
+
+        for (Map.Entry<String, String> temp : runtimeStorage.entrySet()){
+            properties.setProperty(temp.getKey(), temp.getValue());
+        }
+        properties.store(outputStream, null);
+
+    }
+
+    public static void load(InputStream inputStream) throws IOException {
+        //напишите тут ваш код
+       Properties properties = new Properties();
+       properties.load(inputStream);
+
+       for (Map.Entry<Object, Object> loadMap : properties.entrySet()){
+           runtimeStorage.put(loadMap.getKey().toString(), loadMap.getValue().toString());
+       }
+
+
+
+    }
+
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             FileInputStream fos = new FileInputStream(reader.readLine())) {
+            load(fos);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(runtimeStorage);
+    }
+}
